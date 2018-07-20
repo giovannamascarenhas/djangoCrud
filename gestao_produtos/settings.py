7881c9db464c613f5cac3be786f81acd85277d81
Giovanna <Familia Aguiar@FamiliaAguiar>
2018-07-20 10:18:55 -0300

    Criando o site

--- gestao_produtos/settings.py
+++ gestao_produtos/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['geren-produto.herokuapp.com']
+ALLOWED_HOSTS = ['geren-produto.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'produtos',
     'home',
 ]
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'lista_produto'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    'statics',
+]
